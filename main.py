e092ecb3dd07610d83cf03e9b225efa7659f93a6
KayalTV <149043090+KayalTV@users.noreply.github.com>
2023-10-26 12:28:11 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,7 @@
-def recur_factorial(n):  
-
-   if n == 1:  
-
-       return n  
-
-   else:  
-
-       return n*recur_factorial(n-1)  
-
-# take input from the user  
-
-num = int(input("Enter a number: "))  
-
-# check is the number is negative  
-
-if num < 0:  
-
-   print("Sorry, factorial does not exist for negative numbers")  
-
-elif num == 0:  
-
-   print("The factorial of 0 is 1")  
-
-else:  
-
-   print("The factorial of",num,"is",recur_factorial(num))
-
-
+year = 2023
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{0} is a leap year".format(year))
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{0} is a leap year".format(year))
+else:
+    print("{0} is not a leap year".format(year))
